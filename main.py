01f24105382d155d13b624c2b7992800ec53cc2f
phatcvo <vophat0607@gmail.com>
2023-02-05 01:44:18 +0900

    update

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ while True:
     done = False
     obs = env.reset()
     while not done:
+        # Test environment
         action = env.action_type.actions_indexes["IDLE"] 
         obs, reward, done, info = env.step(action)
         print(obs)
